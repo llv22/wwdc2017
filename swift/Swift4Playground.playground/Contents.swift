b272300023b78ec858b77ebfc19d989b96327141
llv23 <llv23@llv23s-Mac.local>
2017-10-09 16:23:04 +0800

    llv23 - for updates of swift with json object serialization

--- swift/Swift4Playground.playground/Contents.swift
+++ swift/Swift4Playground.playground/Contents.swift
@@ -25,20 +25,40 @@ public struct Farm: Codable {
 let farm =  Farm(name: "Old MacDonald’s Farm", location: Location(latitude: 51.6216, longitude: 0.2692), animals: [.cow, .dog, .chicken])
 let payload: Data = try JSONEncoder().encode(farm)
 let decoded_farm = try JSONDecoder().decode(Farm.self, from: payload)
+/// refer to https://stackoverflow.com/questions/29599005/how-to-convert-or-parse-swift-objects-to-json
+/// A generic protocol for creating objects which can be converted to JSON
+//protocol JSONSerializable {
+//    private var dict: [String: Any] { get }
+//}
+//
+//extension JSONSerializable {
+//    /// Converts a JSONSerializable conforming class to a JSON object.
+//    func json() rethrows -> Data {
+//        try JSONSerialization.data(withJSONObject: self.dict, options: nil)
+//    }
+//}
 
 //see: https://stackoverflow.com/questions/28768015/how-to-save-an-array-as-a-json-file-in-swift
 func saveToJsonFile(_ farm: Farm) {
     // Get the url of Persons.json in document directory
     guard let documentDirectoryUrl = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { return }
     let fileUrl = documentDirectoryUrl.appendingPathComponent("Farm.json")
-    
+
     // Transform array into data and save it into file
     do {
-        let data = try JSONSerialization.data(withJSONObject: farm, options: [])
-        try data.write(to: fileUrl, options: [])
+        // see : encoding for json format from swift object by swift 4.0
+        let jsonData = try JSONEncoder().encode(farm)
+        try jsonData.write(to: fileUrl, options: [])
+        //        see : JSONSerialization.data with only json format data
+//        let jsonFormatData =  [["person": ["name": "Dani", "age": "24"]], ["person": ["name": "ray", "age": "70"]]]
+//        let data = try JSONSerialization.data(withJSONObject: jsonFormatData, options: [])
+//        try data.write(to: fileUrl)
     } catch {
         print(error)
     }
 }
 
-saveToJsonFile(farm)
+
+let farm1 =  Farm(name: "Old MacDonald’s Farm", location: Location(latitude: 51.6216, longitude: 0.2692), animals: [.cow, .dog, .chicken])
+saveToJsonFile(farm1)
+
